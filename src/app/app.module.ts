5e4317fe6ae0b7778e158195f3e3a246d4ba918c
machap <paulinhodj10@gmail.com>
2019-02-18 10:53:10 -0300

    Criacao do projeto e instalação da biblioteca de componentes primeng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {ButtonModule, TabViewModule} from 'primeng/primeng';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TabViewModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
