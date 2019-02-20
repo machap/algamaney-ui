84367925b1c43d58cb11f46b737030924845b2f1
machap <paulinhodj10@gmail.com>
2019-02-20 11:04:06 -0300

    Corrido erro na variavel de template no build

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
+  menu = true;
 
-  constructor() { }
-
-  ngOnInit() {
+  exibindoMenu() {
+    if (this.menu) {
+      this.menu = false;
+    } else {
+      this.menu = true;
+    }
   }
-
 }
